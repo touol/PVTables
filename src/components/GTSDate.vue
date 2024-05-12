f86d91c1ae7c372b691534a15d644785f4ee01ea
andy <andystarr1209@gmail.com>
2024-05-12 22:48:30 +0400

    added date component

---
+++ src/components/GTSDate.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Calendar v-model="computedDate" />
+  </div>
+</template>
+
+<script setup>
+import Calendar from "primevue/calendar";
+import { computed } from "vue";
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: ''
+  }
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const computedDate = computed({
+  get() {
+    return props.modelValue.split('-').reverse().join('.')
+  },
+  set(value) {
+    const formattedDate = value.toLocaleDateString('ru-RU').split('.').reverse().join('-')
+    emit('update:modelValue', formattedDate)
+  }
+});
+</script>
+
