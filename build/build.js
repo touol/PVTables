e5e5cbd34e281eb6ad747ccb02ed5ad6c75cd471
Александр Туниеков <touols@yandex.ru>
2024-05-18 13:37:36 +0700

    build

--- build/build.js
+++ build/build.js
@@ -1,16 +1,21 @@
 import { build } from "vite";
 import libraries from './libraries.js'
 import external_packages from './external_packages.json' assert { type: "json" };
-
+import fs from 'fs';
 
 let libraries0 = []
+let external_pvtables = []
 for(let key in libraries){
     libraries0.push({
         entry: libraries[key].entry?libraries[key].entry:libraries[key].path,
         fileName: key,
     })
-    external_packages.push('pvtables/' + key)
+    external_pvtables.push('pvtables/' + key)
 }
+fs.writeFile('./build/external_pvtables.json', JSON.stringify(external_pvtables), function (err) {
+    if (err) throw err;
+    // console.log('It\'s saved!');
+});
 // console.log(libraries0)
 libraries0.forEach(async (lib) => {
     await build({
@@ -22,7 +27,7 @@ libraries0.forEach(async (lib) => {
             },
             emptyOutDir: false,
             rollupOptions: {
-                external: ['vue', 'primevue', 'axios', ...external_packages],
+                external: ['vue', 'primevue', 'axios', ...external_packages, ...external_pvtables],
                 output: {
                     globals: {
                         vue: 'Vue'
